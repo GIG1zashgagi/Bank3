fb0b42290ee14784d1dcd18f2631484e20b0671f
Илья Завадский <ilazavadskij715@gmail.com>
2026-03-16 21:16:07 +0300

    Проверка исключения при отрицательной сумме снятия

--- BankTests/BankAccountTest.cs
+++ BankTests/BankAccountTest.cs
@@ -20,5 +20,20 @@ namespace BankTests
             Assert.AreEqual(expected, actual, 0.001, "Balance after debit is incorrect");
         }
 
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Roman Abramovich", beginningBalance);
+
+            // Act and assert
+            var ex = Assert.ThrowsException<System.ArgumentOutOfRangeException>(
+                () => account.Debit(debitAmount));
+
+            // Assert
+            StringAssert.Contains(ex.Message, "Сумма снятия не может быть отрицательной");
+        }
     }
 }
